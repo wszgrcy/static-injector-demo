682ba0b14b24ca3bce7822f1fc259047495e2739
wszgrcy <wszgrcy@gmail.com>
2021-08-01 10:35:02 +0800

    init

---
+++ webpack.config.ts
@@ -0,0 +1,29 @@
+import * as webpack from 'webpack';
+import * as path from 'path';
+import * as ts from 'typescript';
+import { createTransformer } from 'static-injector/transform';
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              getCustomTransformers: (program: ts.Program) => ({ before: [createTransformer(program)] }),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export default config;
