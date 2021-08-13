16ab7b9b9673a9b0fb5b54eb7b3f322a2319f830
wszgrcy <wszgrcy@gmail.com>
2021-08-13 09:35:18 +0800

    修改为es-module打包

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,13 @@
-import * as webpack from 'webpack';
-import * as path from 'path';
-import * as ts from 'typescript';
-import { createTransformer } from 'static-injector/transform';
+import * as webpack from "webpack";
+import * as path from "path";
+import * as ts from "typescript";
+import { createTransformer } from "static-injector/transform";
 const config: webpack.Configuration = {
-  mode: 'production',
-  entry: './src/index.ts',
+  mode: "production",
+  entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
   module: {
     rules: [
@@ -15,8 +15,9 @@ const config: webpack.Configuration = {
         test: /.ts$/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
+              configFile: "tsconfig.build.json",
               getCustomTransformers: (program: ts.Program) => ({ before: [createTransformer(program)] }),
             },
           },
